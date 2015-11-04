77e9f244832c3b15c54220d8467b1b0a8b297149
Landon Grim <lgrimosu@gmail.com>
2015-11-03 23:09:02 -0500

    Finish adding Bar

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,4 +1,26 @@
 class BarsController < ApplicationController
-  def new
+  def show
+    @bar = Bar.find(params[:id])
   end
+
+  def new
+    @bar = Bar.new
+  end
+
+  def create
+    @bar = Bar.new(bar_params)
+    if @bar.save
+      flash[:success] = "Succesfully added Bar!"
+      redirect_to @bar
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def bar_params
+    params.require(:bar).permit(:name, :address)
+  end
+
 end
