a065e727b861c84203b1325c19e0c48b18c0e0d1
Landon Grim <lgrimosu@gmail.com>
2015-11-08 17:04:48 -0500

    added flash messages, can edit bars now

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,5 +1,5 @@
 class BarsController < ApplicationController  
-  
+
   def destroy
     Bar.find(params[:id]).destroy
     flash[:success] = "Bar Deleted"
@@ -21,17 +21,31 @@ class BarsController < ApplicationController
   def create
     @bar = Bar.new(bar_params)
     if @bar.save
-      redirect_to @bar
       flash[:success] = "Succesfully added Bar!"
+      redirect_to @bar
     else
       render 'new'
     end
   end
 
-  private
+  def edit
+    @bar = Bar.find(params[:id])
+  end
 
+
+  def update
+    @bar = Bar.find(params[:id])
+    if @bar.update_attributes(bar_params)
+      flash[:success] = "Bar updated"
+      redirect_to @bar
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
   def bar_params
     params.require(:bar).permit(:name, :address)
   end
-
 end
