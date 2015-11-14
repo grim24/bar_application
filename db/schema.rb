5ba39b9c0753b99ded0e2e15632e0052e767461e
Landon Grim <lgrimosu@gmail.com>
2015-11-14 16:22:33 -0500

    added users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109163341) do
+ActiveRecord::Schema.define(version: 20151114212107) do
 
   create_table "bars", force: :cascade do |t|
     t.string   "name"
@@ -32,5 +32,13 @@ ActiveRecord::Schema.define(version: 20151109163341) do
 
   add_index "deals", ["bar_id", "created_at"], name: "index_deals_on_bar_id_and_created_at"
   add_index "deals", ["bar_id"], name: "index_deals_on_bar_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
 
 end
